6c26b2fc68f7575e07f37a6eaa5646cf65ff0663
Sidharth075 <99586419+Sidharth075@users.noreply.github.com>
2022-05-02 20:18:17 +0530

    Create W3 q1.c

---
+++ W3 q1.c
@@ -0,0 +1,18 @@
+ #include<stdio.h>
+
+int  gcd(int c, int d) 
+{
+    if (d != 0)
+        return gcd(d, c % d);
+    else
+        return n1;
+}
+
+int main() 
+{
+    int a, b;
+    printf("Enter any two positive numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("GCD of %d and %d is %d.", a, b, gcd(a, b));
+    return 0;
+}
